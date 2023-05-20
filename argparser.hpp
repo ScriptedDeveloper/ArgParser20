467008a0f44cfd1eb5abbd3587c33bc4df345a92
Your Name <you@example.com>
2023-05-20 19:01:46 +0200

    Added some basic functionality

--- argparser.hpp
+++ argparser.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <stdexcept>
 #include <string_view>
 #include <unordered_map>
 #include <variant>
@@ -17,8 +18,8 @@ class ArgParser {
 		 */
 		struct Param {
 			bool has_param{};
-			AnyVar title{};
 			AnyVar desc{};
+			AnyVar value{}; // value of param, like --input file.txt, file.txt is the value
 		};
 
 	public:
@@ -37,15 +38,56 @@ class ArgParser {
 		Param p;
 		p.has_param = has_param;
 		p.desc = desc;
-		p.title = title;
-		cmdMap[param_key] = p;
+		if(is_string_type<T>() && std::holds_alternative<const char*>(param_key)) {
+			auto param_str = std::string_view(std::get<const char*>(param_key));
+			title_map[std::string_view(title)] = param_str;
+			cmdMap[param_str] = p;
+		}
+		else if(is_string_type<T>()) {
+			title_map[std::string_view(title)] = param_key;
+			cmdMap[param_key] = p;
+		} else {
+			title_map[title] = param_key;
+			cmdMap[param_key] = p;
+		}
+	}
+	/*
+	 * Checks if the command line argument exists.
+	 */
+	template <typename T>
+	constexpr bool has_Option(T title) {
+		return title_map.find(title) != title_map.end();
+	}
+	/*
+	 * Gets command line option by value.
+	 */
+	auto getOptionValue(AnyVar title) {
+		/*
+		 * First, we convert char* or const char * to string_view, so we have 1 unique type to check after
+		 */
+		if(std::holds_alternative<char*>(title))
+			title = std::string_view(std::get<char*>(title));
+		else if(std::holds_alternative<const char*>(title))
+			title = std::string_view(std::get<const char*>(title));
+		if(!has_Option(title)) {
+			PrintVariant(title);
+			throw std::invalid_argument("Error! Expected valid Option, but got non-existing.");
+		}	
+		if(!parsedArgs)
+			parse(); // First we have to of course parse our command line arguments before we can do anything with the value.
+		Param member = cmdMap[title_map[title]];
+		if(!member.has_param) {
+			PrintVariant(title);
+			throw std::invalid_argument("Error! Specified command line argument doesn't require a param.");
+		}
+		return member.value;
 	}
 		
 	private:	
 		char **argv{};
 		int argc{};
 		/*
-		 * This function checks if the function is this specific type.
+		 * This function checks if the function is this specific type, by value.
 		 */
 		template <typename T, typename type>
 		bool is_type(T val) {
@@ -54,7 +96,6 @@ class ArgParser {
 		/*
 		 * This function gets the orignal function type.
 		 */
-	
 		AnyVar get_value(std::string_view var) {
 			if(is_type<std::string_view, int>(var)) 
 				return std::stoi(var.data());
@@ -65,19 +106,52 @@ class ArgParser {
 			return var;
 		}
 		/*
+		 * This function simply prints out the variant gotten by std::visit.
+		 */
+		void PrintVariant(AnyVar &val) {
+			std::visit([](auto &val){
+				std::cout << std::endl << "Argument : " << val << std::endl;
+			}, val);
+		}
+		template <typename T>
+		constexpr bool is_string_type() {
+		   return std::is_same_v<T, const char*> ||
+		   std::is_same_v<T, std::string> ||
+		   std::is_same_v<T, std::string_view> ||
+		   std::is_same_v<T, char*>;
+		}
+		/*
 		 * We store the command line parameters in an unordered_map, an ordered map is not needed here
 		 */
 		std::unordered_map<AnyVar, Param> cmdMap{};
 		/*
+		 * We also store the titles and map them to the params.
+		 */
+		std::unordered_map<AnyVar, AnyVar> title_map{};
+		/*
+		 * Indicator if we already parsed the cli arguments or not.
+		 */
+		bool parsedArgs{false};
+		/*
 		 * This function parses argv params into the map.
 		 */
 		bool parse() {
+			Param *last_param;
 			for(int i = 1; i < argc; i++) {
 				auto val = get_value(std::string_view(argv[i])); // getting orignal value type from char *
 				auto it_param = cmdMap.find(val);
-				if(it_param == cmdMap.end())
-					throw std::invalid_argument("Expected valid iterator but got nothing");
+				if(it_param == cmdMap.end()) {
+					if(last_param->has_param)
+						last_param->value = val;
+					else
+						throw std::invalid_argument("Wrong order of command line arguments, expected value but got something else");
+				} else {
+					//PrintVariant(val);
+					//throw std::invalid_argument("Expected new command line argument but got the same argument twice.");
+					last_param = &it_param->second;
+				}
 			}
+			parsedArgs = true;
 			return true;
 		}
 };
--- test.cpp
+++ test.cpp
@@ -3,5 +3,8 @@
 
 int main(int argc, char **argv) {
 	ArgParser p(argc, argv);
-	p.addOption("erd", "nice", "ok", false);
+	p.addOption("Help", "-h", "Shows output", true);
+	// TODO:
+	// Need to remove this extra std::get call, make it handled by the library somehow
+	std::cout <<  std::get<std::string_view>(p.getOptionValue("Help")) << std::endl;
 }
